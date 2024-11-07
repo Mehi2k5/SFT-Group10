0d9da293eec4c83814d67772b36a9b1a1d47dc86
Nguyen Duong Truong Phuc <nguyenduongtruongphuc@gmail.com>
2024-11-06 22:31:18 -0500

    Update extraFunctions.h

--- MS3/extraFunctions.h
+++ MS3/extraFunctions.h
@@ -3,21 +3,21 @@
 #include "mapping.h" 
 
 // CUSTOM FUNCTIONS
-/**
+/*
  * Adding a package to the truck if it fits within the truck's remaining capacity
  * @parameter tr: Pointer to the truck where the package will be added
  * @parameter pkg: The package to add the weight and the size
  */
 void addPackage(struct Truck* tr, struct Package pkg); 
 
-/**
+/*
  * Calculating the remaining capacity of a truck
  * @parameter tr: Pointer to the truck
  * @return Remaining capacity as a percentage
  */
 double calculateCapacity(const struct Truck* tr);
 
-/**
+/*
  * Calculating the shortest path from the start point to the destination on mapp
  * @parameter m: Pointer to the map
  * @parameter begin: Starting point
@@ -26,7 +26,7 @@ double calculateCapacity(const struct Truck* tr);
  */
 struct Path calculatePath(const struct Map* m, struct Point begin, struct Point dest);
 
-/**
+/*
  * Checking if the package can be added to the truck or not
  * @parameter tr: Pointer to the truck
  * @parameter pkg: The package to check
@@ -34,7 +34,7 @@ struct Path calculatePath(const struct Map* m, struct Point begin, struct Point 
  */
 int canAddPackage(const struct Truck* tr, const struct Package* pkg);
 
-/**
+/*
  * Counting the number of packages on the truck now
  * @parameter tr: Pointer to the truck
  * @return the number of packages on the truck 
